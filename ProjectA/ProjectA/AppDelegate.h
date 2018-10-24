b80b97c5f737efc2600687fa5891e86376653a2e
qinjun447 <qinjun447@pingan.com.cn>
2018-10-24 16:04:21 +0800

    上传工程文件

---
+++ ProjectA/ProjectA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectA
+//
+//  Created by Mike on 2018/10/24.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectA/ProjectA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectA
+//
+//  Created by Mike on 2018/10/24.
+//  Copyright © 2018 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
